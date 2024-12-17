324277d306271b5e18991bbb9db64d94e71605b6
dimma-18 <Dimma-18@yandex.ru>
2024-12-17 05:42:25 +0300

    АиСД  QuickSort

---
+++ QuickSort.go
@@ -0,0 +1,46 @@
+package main
+
+func QuickSortLomuto(data []int, l int, r int) {
+	if l >= r {
+		return
+	}
+	pivot := data[r]
+
+	i := l - 1
+	for j := l; j < r; j++ {
+		if data[j] <= pivot {
+			i++
+			data[i], data[j] = data[j], data[i]
+		}
+	}
+	data[i+1], data[r] = data[r], data[i+1]
+
+	QuickSortLomuto(data, l, i)
+	QuickSortLomuto(data, i+2, r)
+}
+
+func QuickSortHoare(data []int, l int, r int) {
+	if l >= r {
+		return
+	}
+	pivot := data[r] // l || (l + r) / 2 || r || random
+
+	i := l
+	j := r
+	for i < j {
+		for data[i] < pivot {
+			i++
+		}
+		for data[j] > pivot {
+			j--
+		}
+		if i <= j {
+			data[i], data[j] = data[j], data[i]
+			i++
+			j--
+		}
+	}
+
+	QuickSortHoare(data, l, j)
+	QuickSortHoare(data, i, r)
+}
--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 )
 
 func main() {
-	data := make([]int, 0, 100000000)
+	data := make([]int, 0, 1000000)
 	goodData := make([]int, cap(data))
 	for i := 0; i < cap(data); i++ {
 		data = append(data, rand.Int())
 		goodData[i] = data[i]
 	}
-	MergeSort(data, 0, len(data))
+	QuickSortLomuto(data, 0, len(data)-1)
+	//QuickSortHoare(data, 0, len(data)-1)
 	slices.Sort(goodData)
 	fmt.Println(slices.Equal(data, goodData))
 }
