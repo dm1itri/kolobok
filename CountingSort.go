05c56192c5388a04f48d880224eb5d5e82a7320c
dimma-18 <Dimma-18@yandex.ru>
2024-12-17 06:30:46 +0300

    АиСД Base CountingSort

--- CountingSort.go
+++ CountingSort.go
@@ -1,27 +1,20 @@
 package main
 
-import "slices"
-
-func GetNormValue(value int, minValue int) int {
-	return value - minValue
-}
-
 func CountingSort(data []int) {
-	minData := slices.Min(data)
-	counts := make([]int, GetNormValue(slices.Max(data), minData)+1)
-	for i := 0; i < len(counts); i++ {
+	var counts [1000000]int
+	for i := 0; i < 1000000; i++ {
 		counts[i] = 0
 	}
 	for i := 0; i < len(data); i++ {
-		counts[GetNormValue(data[i], minData)]++
+		counts[data[i]]++
 	}
-	for i := 1; i < len(counts); i++ {
+	for i := 1; i < 1000000; i++ {
 		counts[i] = counts[i] + counts[i-1]
 	}
 	dataCopy := make([]int, len(data))
 	for i := len(data) - 1; i >= 0; i-- {
-		dataCopy[counts[GetNormValue(data[i], minData)]-1] = data[i]
-		counts[GetNormValue(data[i], minData)]--
+		dataCopy[counts[data[i]]-1] = data[i]
+		counts[data[i]]--
 	}
 	for i := 0; i < len(data); i++ {
 		data[i] = dataCopy[i]
